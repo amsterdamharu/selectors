78141f893aa4a3ad92318bc68370a2f7aaeaaaf2
Harm Meijer <harm.meijer@commercetools.com>
2022-02-19 18:34:51 +0100

    wut

--- src/store.js
+++ src/store.js
@@ -8,9 +8,56 @@ const defaultState = {
     ],
   },
 };
-
+const middleWares = [
+  (store) => (next) => (action) =>
+    console.log('first', action) ||
+    next({ type: 'changed' }),
+  (store) => (next) => (action) =>
+    console.log('second', action) || next(action),
+];
+//this is what redux devtools returns, how is it called by redux?
+const myCreateStore = (cs) => {
+  console.log('ok, was called with:', cs);
+  return (reducer, initialState) => {
+    console.log('ignore original?', cs);
+    let state = initialState;
+    const subscriptions = new Map();
+    const getState = () => initialState;
+    const dispatch = (action) => {
+      mw(action);
+    };
+    const mw = middleWares
+      .map((mw) => mw({ getState, dispatch }))
+      .reverse()
+      .reduce(
+        (acc, mw) => mw(acc),
+        (action) => {
+          console.log('got action:', action);
+          state = reducer(state, action);
+          subscriptions.forEach((fn) => fn());
+        }
+      );
+    const subscribe = (fn) => {
+      subscriptions.set(fn, fn);
+      return () => {
+        console.log('unsubbed');
+        subscriptions.delete(fn);
+      };
+    };
+    return {
+      getState,
+      dispatch,
+      subscribe,
+    };
+  };
+};
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+//     applyMiddleware(...middleware)
+//   ));
 const store = createStore(
-  (state = defaultState) => {
+  (state = defaultState, action) => {
+    console.log('in reducer:', state, action);
     //reducer always returns a new state object but
     //  never change values this means components will
     //  never re render but selectors will be called
@@ -18,8 +65,8 @@ const store = createStore(
     return { ...state };
   },
   defaultState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  myCreateStore
 );
-
+window.store = store;
+store.dispatch({ type: 'ok' });
 export default store;
