0d194473787e270239253bf3c05ae66f57532119
Harm Meijer <harm.meijer@commercetools.com>
2022-02-19 22:32:37 +0100

    ok?

--- src/store.js
+++ src/store.js
@@ -8,53 +8,6 @@ const defaultState = {
     ],
   },
 };
-const middleWares = [
-  (store) => (next) => (action) =>
-    console.log('first', action) ||
-    next({ type: 'changed' }),
-  (store) => (next) => (action) =>
-    console.log('second', action) || next(action),
-];
-//this is what redux devtools returns, how is it called by redux?
-const myCreateStore = (cs) => {
-  console.log('ok, was called with:', cs);
-  return (reducer, initialState) => {
-    console.log('ignore original?', cs);
-    let state = initialState;
-    const subscriptions = new Map();
-    const getState = () => initialState;
-    const dispatch = (action) => {
-      mw(action);
-    };
-    const mw = middleWares
-      .map((mw) => mw({ getState, dispatch }))
-      .reverse()
-      .reduce(
-        (acc, mw) => mw(acc),
-        (action) => {
-          console.log('got action:', action);
-          state = reducer(state, action);
-          subscriptions.forEach((fn) => fn());
-        }
-      );
-    const subscribe = (fn) => {
-      subscriptions.set(fn, fn);
-      return () => {
-        console.log('unsubbed');
-        subscriptions.delete(fn);
-      };
-    };
-    return {
-      getState,
-      dispatch,
-      subscribe,
-    };
-  };
-};
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
-//     applyMiddleware(...middleware)
-//   ));
 const store = createStore(
   (state = defaultState, action) => {
     console.log('in reducer:', state, action);
@@ -64,9 +17,64 @@ const store = createStore(
     //  every time an action dispatches
     return { ...state };
   },
-  defaultState,
-  myCreateStore
+  defaultState
 );
-window.store = store;
-store.dispatch({ type: 'ok' });
+const middleWares = [
+  (store) => (next) => (action) =>
+    console.log('first', action) ||
+    next({ type: 'changed' }),
+  (store) => (next) => (action) =>
+    console.log('second', action) || next(action),
+];
+const reducer = (state = defaultState, action) => {
+  console.log('in reducer:', state, action);
+  return state;
+};
+const myCreateStore = (reducer, initialState) => {
+  let state = initialState;
+  const subscriptions = new Map();
+  const getState = () => initialState;
+  const dispatch = (action) => {
+    console.log('got action:', action);
+    state = reducer(state, action);
+    subscriptions.forEach((fn) => fn());
+  };
+  const subscribe = (fn) => {
+    subscriptions.set(fn, fn);
+    return () => {
+      console.log('unsubbed');
+      subscriptions.delete(fn);
+    };
+  };
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+};
+function applyMiddleware(...wares) {
+  return function (createStore) {
+    return function () {
+      const store = createStore.apply(void 0, arguments);
+
+      const middlewareAPI = {
+        getState: store.getState,
+        dispatch: store.dispatch,
+      };
+      const _dispatch = wares
+        .map((mw) => mw(middlewareAPI))
+        .reverse()
+        .reduce((acc, mw) => mw(acc), store.dispatch);
+
+      return { ...store, dispatch: _dispatch };
+    };
+  };
+}
+const wut = applyMiddleware(...middleWares)(myCreateStore)(
+  reducer
+);
+console.log('before dispatch');
+window.wut = wut;
+wut.dispatch({ type: 'ok' });
+console.log('done dispatch');
 export default store;
