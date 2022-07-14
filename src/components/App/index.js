003a659901d7ad4c84922bb792b3534efcb7237f
Harm Meijer <harm.meijer@commercetools.com>
2022-07-14 19:44:57 +0200

    no api key

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { check } from './tokenAction';
 
 export default function App() {
-  return <div>hello world</div>;
+  const [response, setResponse] = useState({});
+  const click = useCallback(
+    () =>
+      check().then(
+        (response) => setResponse(response),
+        (error) => console.log('some error:', error)
+      ),
+    []
+  );
+  return (
+    <div>
+      <div>
+        <button onClick={click}>get token</button>
+      </div>
+      <div>
+        <pre>{JSON.stringify(response, undefined, 2)}</pre>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/App/tokenAction.js
@@ -0,0 +1,61 @@
+import { encode } from 'js-base64';
+import axios from 'axios';
+
+const createAuth = (au) => {
+  console.log(
+    `XXXXX base solved with hack: ${typeof encode}`
+  );
+  return encode(`${au.id}:${au.secret}`);
+};
+
+const getToken = (au) => () => {
+  const scope = au.scope;
+  const auth = createAuth(au);
+
+  const instance = axios.create({
+    timeout: 1000,
+    headers: {
+      authorization: `Basic ${auth}`,
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+  });
+  const formData = new URLSearchParams({
+    grant_type: 'client_credentials',
+    scope,
+  });
+  return instance
+    .post(
+      `${au.authUrl}/oauth/${au.projectKey}/anonymous/token`,
+      //is axios bugging out on me, works differently in fetch
+      // `grant_type=client_credentials&scope=${scope}`,
+      // `grant_type=client_credentials&scope=manage_project%3Afrontastic-1`,
+      formData
+    )
+    .then((response) =>
+      response.ok
+        ? response.json()
+        : Promise.reject(response)
+    )
+    .catch((e) => console.log('XXXXXX error 88:', e));
+};
+
+export const check = async () => {
+  const settings = {
+    clientId: 88,
+    clientSecret: 88,
+    scope: 88,
+    projectKey: 88,
+    authUrl: 88,
+  };
+  const au = {
+    id: settings.clientId,
+    secret: settings.clientSecret,
+    scope: settings.scope,
+    projectKey: settings.projectKey,
+    authUrl: settings.authUrl,
+  };
+  return getToken(au)().then((token) => {
+    console.log('XXXXX setting token:', token);
+    return token;
+  });
+};
